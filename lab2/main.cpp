7914088cc345addc1624eeb4d1f3613a76c1086a
Maarten Terpstra <m.l.terpstra@student.rug.nl>
2015-02-16 13:42:14 +0100

    After a long fight, we managed to make it draw squares

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <math.h>
+#include <iostream>
 
 #include <GL/glew.h>
 #include <GL/glut.h>
@@ -13,6 +14,14 @@
 
 CGcontext	context;
 CGprofile	vertexProfile, fragmentProfile;
+CGprogram   vertexProgram;
+float       near_val, bottom_val, top_val; 
+CGparameter modelView = NULL;
+CGparameter modelViewProj = NULL;
+CGparameter wsize = NULL;
+CGparameter near = NULL;
+CGparameter top = NULL;
+CGparameter bottom = NULL;
 
 int w_width=512, w_height=512;
 
@@ -37,6 +46,45 @@ void keyboard(unsigned char key, int x, int y)
     }
 }
 
+void glhFrustumf2(float *matrix, float left, float right, float bottom, float top,
+                  float znear, float zfar)
+{
+    near_val = znear;
+    bottom_val = bottom;
+    top_val = top;
+    float temp, temp2, temp3, temp4;
+    temp = 2.0 * znear;
+    temp2 = right - left;
+    temp3 = top - bottom;
+    temp4 = zfar - znear;
+    matrix[0] = temp / temp2;
+    matrix[1] = 0.0;
+    matrix[2] = 0.0;
+    matrix[3] = 0.0;
+    matrix[4] = 0.0;
+    matrix[5] = temp / temp3;
+    matrix[6] = 0.0;
+    matrix[7] = 0.0;
+    matrix[8] = (right + left) / temp2;
+    matrix[9] = (top + bottom) / temp3;
+    matrix[10] = (-zfar - znear) / temp4;
+    matrix[11] = -1.0;
+    matrix[12] = 0.0;
+    matrix[13] = 0.0;
+    matrix[14] = (-temp * zfar) / temp4;
+    matrix[15] = 0.0;
+}
+
+
+void glhPerspectivef2(float *matrix, float fovyInDegrees, float aspectRatio,
+                      float znear, float zfar)
+{
+    float ymax, xmax;
+    ymax = znear * tanf(fovyInDegrees * M_PI / 360.0);
+    xmax = ymax * aspectRatio;
+    glhFrustumf2(matrix, -xmax, xmax, -ymax, ymax, znear, zfar);
+}
+
 void handleCgError() 
 {
     fprintf(stderr, "Cg error: %s\n", cgGetErrorString(cgGetError()));
@@ -47,28 +95,47 @@ void display()
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
-    
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(30.0f, (float)w_width/w_height, 0.1, 100.0);
+    float matrix[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    glhPerspectivef2(matrix, 30.0f, (float)w_width/w_height, 0.1, 100.0);   
+    glMultMatrixf(matrix);
     
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(4, 0, -3, 0, 0, 0, 0, 1, 0);
 
+    //Enable this so OpenGL listens to PSIZE returned by CG
+    glEnable(GL_VERTEX_PROGRAM_POINT_SIZE_ARB);
+
+    //Pass relevant parameters to CG
+    cgGLSetStateMatrixParameter(modelView, CG_GL_MODELVIEW_MATRIX, CG_GL_MATRIX_IDENTITY);
+    cgGLSetStateMatrixParameter(modelViewProj, CG_GL_MODELVIEW_PROJECTION_MATRIX, CG_GL_MATRIX_IDENTITY);
+    cgGLSetParameter2f(wsize, glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+    cgGLSetParameter1f(near, near_val);
+    cgGLSetParameter1f(top, top_val);
+    cgGLSetParameter1f(bottom, bottom_val);
+
     //Select coordinates, color and vectors(?) from points
     glClientActiveTexture(GL_TEXTURE0);
-    glTexCoordPointer(3, GL_FLOAT, sizeof(Surfel), &pts[0].uvec);
+    glTexCoordPointer(4, GL_FLOAT, sizeof(Surfel), &pts[0].color);
     glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 
     glClientActiveTexture(GL_TEXTURE1);
+    glTexCoordPointer(3, GL_FLOAT, sizeof(Surfel), &pts[0].uvec);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+    glClientActiveTexture(GL_TEXTURE2);
     glTexCoordPointer(3, GL_FLOAT, sizeof(Surfel), &pts[0].vvec);
     glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 
     glVertexPointer(3, GL_FLOAT, sizeof(Surfel), &pts[0].pos);
-    glColorPointer(3, GL_FLOAT, sizeof(Surfel), &pts[0].color);
     glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_COLOR_ARRAY);
+
+
+    //Bind and load the shader
+    cgGLEnableProfile(vertexProfile);
+    cgGLBindProgram(vertexProgram);
 
     //Actually draw the thing
     glDrawArrays(GL_POINTS, 0, numpoints);
@@ -78,9 +145,12 @@ void display()
     glDisableClientState(GL_TEXTURE_COORD_ARRAY);
     glClientActiveTexture(GL_TEXTURE1);
     glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    glClientActiveTexture(GL_TEXTURE2);
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
     glDisableClientState(GL_VERTEX_ARRAY);
     glDisableClientState(GL_COLOR_ARRAY);
-    
+
+    cgGLDisableProfile(vertexProfile);    
     glutSwapBuffers();
 }
 
@@ -122,7 +192,13 @@ CGprogram loadCgProgram(CGprofile profile, const char *filename)
 
 void loadCgPrograms()
 {
-    /* Load all Cg programs that are used with loadCgProgram */
+    vertexProgram = loadCgProgram(vertexProfile, "pointscalingvertex.cg");
+    modelView = cgGetNamedParameter(vertexProgram, "modelView");
+    modelViewProj = cgGetNamedParameter(vertexProgram, "modelViewProj");
+    wsize = cgGetNamedParameter(vertexProgram, "wsize");
+    top = cgGetNamedParameter(vertexProgram, "top");
+    bottom = cgGetNamedParameter(vertexProgram, "bottom");
+    near = cgGetNamedParameter(vertexProgram, "near");
 }
 
 void idle()
